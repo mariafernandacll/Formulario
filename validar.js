df7df5926961a93edffd1eddb1585281bc561032
mariafernandacll <mariafernandacll@hotmail.com>
2021-09-28 22:27:48 -0500

    Commit js2

--- validar.js
+++ validar.js
@@ -1,5 +1,3 @@
-
-document.write("hola")
 function validar_formulario (){
     //1.Accedemos a los elementos del formulario
     var usuario = document.getElementById('username');
@@ -10,6 +8,18 @@ function validar_formulario (){
     //2.Aplicar validaciones
     if (!usuario.checkValidity()) {
         mensaje += "Usuario requerido y longitud minima 8 caracteres";
+    }
+           
+    if (!correo.checkValidity()) {
+        mensaje += "Correo requerido con formato xxx@xxx.xx";
+    }
+        
+    if (!contraseña.checkValidity()) {
+        mensaje += "Contraseña requerida con formato xxx@xxx.xx";
+    }
+    
+    if (mensaje == "") {
+        mensaje = "Todos los campos de formulario son validos";
     }
     alert(mensaje);
 
